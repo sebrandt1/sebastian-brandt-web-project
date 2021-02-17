78622f40dffd21f520863f05fc64126edeb6815b
SPEEDYGONZALES\Sebastian <s.ebrandt@hotmail.com>
2021-02-17 19:31:30 +0100

    fetchRandomFact no longer returns a value since it modifies the innerHTML of the fact text box

--- scripts.js
+++ scripts.js
@@ -2,11 +2,9 @@ const url = "https://catfact.ninja/fact";
 
 
 async function fetchRandomFact() {
-    let jsonData = [];
-
     await fetch(url)
-        .then(response => response.json())
-        .then(json => document.getElementById('fact').innerHTML = json.fact);
-
-    return jsonData.fact;
+        .then(response => 
+            response.json())
+        .then(json => 
+            document.getElementById('fact').innerHTML = json.fact);
 }
