b7036f8fe2781a28a95ac7d0c5ef9b6d2b62091e
SPEEDYGONZALES\Sebastian <s.ebrandt@hotmail.com>
2021-02-19 17:01:44 +0100

    bugfix for sign out function

--- scripts.js
+++ scripts.js
@@ -17,7 +17,7 @@ function onSignIn(googleUser) {
 
 function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut();
+    auth2.disconnect();
 }
 
 function displayGoogleName() {
