bbf5a53e04102d172cbcfe561a01717fab58dd73
SPEEDYGONZALES\Sebastian <s.ebrandt@hotmail.com>
2021-02-19 17:06:21 +0100

    googleName is now global

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 const url = "https://catfact.ninja/fact";
 const CLIENT_ID = "201551229723-hv19qdfaf6tstadb3p5q9qfu0nfr84nf.apps.googleusercontent.com";
-var googleName = "";
+googleName = "";
 
 async function fetchRandomFact() {
     await fetch(url)
