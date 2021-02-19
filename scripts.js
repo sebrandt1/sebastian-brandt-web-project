bcae3a7f67d019c47cfa22729433cdc34ccefedd
SPEEDYGONZALES\Sebastian <s.ebrandt@hotmail.com>
2021-02-19 17:15:42 +0100

    another test to see if username will be displayed

--- scripts.js
+++ scripts.js
@@ -2,8 +2,6 @@ const url = "https://catfact.ninja/fact";
 const CLIENT_ID = "201551229723-hv19qdfaf6tstadb3p5q9qfu0nfr84nf.apps.googleusercontent.com";
 googleName = "";
 
-setInterval(displayGoogleName, 5000);
-
 async function fetchRandomFact() {
     await fetch(url)
         .then(response => 
@@ -15,6 +13,7 @@ async function fetchRandomFact() {
 function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
     googleName = profile.getGivenName();
+    document.getElementById('welcomemsg').innerHTML = googleName;
 }
 
 function signOut() {
