5a3115b7b95ca1762595877060be103f17faf899
SPEEDYGONZALES\Sebastian <s.ebrandt@hotmail.com>
2021-02-19 16:51:44 +0100

    added script to display google first name of currently signed in user

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
 const url = "https://catfact.ninja/fact";
 const CLIENT_ID = "201551229723-hv19qdfaf6tstadb3p5q9qfu0nfr84nf.apps.googleusercontent.com";
+var googleName = "";
 
 async function fetchRandomFact() {
     await fetch(url)
@@ -11,9 +12,14 @@ async function fetchRandomFact() {
 
 function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
+    googleName = profile.getGivenName();
 }
 
 function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut();
+}
+
+function displayGoogleName() {
+    document.getElementById('welcomemsg').innerHTML = googleName;
 }
