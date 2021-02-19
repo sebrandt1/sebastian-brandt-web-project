16b83e48be1d8a2a8aed1da9d452bde998c5170b
SPEEDYGONZALES\Sebastian <s.ebrandt@hotmail.com>
2021-02-19 17:13:41 +0100

    script test to see if displaying google name works

--- scripts.js
+++ scripts.js
@@ -2,6 +2,8 @@ const url = "https://catfact.ninja/fact";
 const CLIENT_ID = "201551229723-hv19qdfaf6tstadb3p5q9qfu0nfr84nf.apps.googleusercontent.com";
 googleName = "";
 
+setInterval(displayGoogleName, 5000);
+
 async function fetchRandomFact() {
     await fetch(url)
         .then(response => 
